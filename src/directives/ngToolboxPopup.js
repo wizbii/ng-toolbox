fa509697e4fb24b445cea97056e84946dac7ebd1
Gabin <hello@gabinaureche.com>
2015-11-12 15:40:45 +0100

    feat(ngToolboxPopup): add popup directive

---
+++ src/directives/ngToolboxPopup.js
@@ -0,0 +1,49 @@
+/* global _ */
+
+function ngToolboxPopup () {
+  const DEFAULT_WIDTH = 600
+  const DEFAULT_HEIGHT = 300
+
+  function configToString (config) {
+    return _.reduce(config, function (configList, value, name) {
+      configList.push(`${name}=${value}`)
+      return configList
+    }, []).join(', ')
+  }
+
+  return {
+    restrict: 'A',
+    scope: { width: '@ngToolboxPopupWidth', height: '@ngToolboxPopupHeight' },
+    link (scope, element, attrs) {
+      element.on('click', function (event) {
+        const width = scope.width || DEFAULT_WIDTH
+        const height = scope.height || DEFAULT_HEIGHT
+
+        const position = {
+          left: (window.screen.width / 2) - (width / 2),
+          top: (window.screen.height / 2) - (height / 2)
+        }
+
+        const config = {
+          toolbar: 'no',
+          location: 'no',
+          directories: 'no',
+          status: 'no',
+          menubar: 'no',
+          scrollbars: 'yes',
+          resizable: 'no',
+          copyhistory: 'no',
+          width: width,
+          height: height,
+          top: position.top,
+          left: position.left
+        }
+
+        window.open(attrs.href, attrs.title || '', configToString(config))
+        event.preventDefault()
+      })
+    }
+  }
+}
+
+export default ngToolboxPopup
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
 import './components/dropdown'
 import truncate from './filters/truncate'
 import Loader from './factories/Loader'
+import ngToolboxPopup from './directives/ngToolboxPopup.js'
 
 export default
   angular
     .module('ng-toolbox', ['ng-toolbox-dropdown'])
     .filter({ truncate })
     .factory({ Loader })
+    .directive({ ngToolboxPopup })
