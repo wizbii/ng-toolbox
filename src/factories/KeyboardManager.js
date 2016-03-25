c87d65755fdda40ac309840d7af01c7fd7c79d98
Bertrand Darbon <bertrand.darbon@gmail.com>
2016-03-25 15:06:30 +0100

    fix(KeyboardManager): fix lodash dependencies

--- src/factories/KeyboardManager.js
+++ src/factories/KeyboardManager.js
@@ -1,8 +1,9 @@
-/* global _ */
+import mapKeys from 'lodash/mapKeys'
+
 function KeyboardManager () {
   return function (element, handlers) {
     const mapping = { down: 40, up: 38, enter: 13, tab: 9, esc: 27 }
-    handlers = _.mapKeys(handlers, (handler, keyName) => mapping[keyName])
+    handlers = mapKeys(handlers, (handler, keyName) => mapping[keyName])
 
     element[0].addEventListener('keydown', function (...args) {
       const handler = handlers[args[0].keyCode]
