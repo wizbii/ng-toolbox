07baf7b76ac100c60c6581f6684e3a00a23789fd
Gabin <hello@gabinaureche.com>
2016-08-23 23:45:49 +0200

    feat(KeyboardManager): return un-register callback

--- src/modules/factories/KeyboardManager.js
+++ src/modules/factories/KeyboardManager.js
@@ -1,16 +1,30 @@
 import mapKeys from 'lodash/mapKeys'
 
 function KeyboardManager () {
-  return function (element, handlers) {
-    const mapping = { down: 40, up: 38, enter: 13, tab: 9, esc: 27 }
+  return function ($element, handlers) {
+    const mapping = {
+      down: 40,
+      up: 38,
+      enter: 13,
+      tab: 9,
+      esc: 27
+    }
+
     handlers = mapKeys(handlers, (handler, keyName) => mapping[keyName])
 
-    element[0].addEventListener('keydown', function (...args) {
-      const handler = handlers[args[0].keyCode]
-      if (handler == null) return
+    const listener = (...args) => {
+      const [event] = args
+      const handler = handlers[event.keyCode]
+
+      if (handler == null) {
+        return
+      }
 
       handler(...args)
-    }, true)
+    }
+
+    $element.on('keydown', listener)
+    return () => $element.off('keydown', listener)
   }
 }
 
